49202a5311404de53125a457fc229ecd459e380f
behub (torch) <ben@behub.com>
2021-08-30 14:55:12 +1000

    Initial

---
+++ Soundpaths.lua
@@ -0,0 +1,26 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cff00ffffRealm - Inssurance:RealSlow|r", [[Interface\Addons\SharedMedia_Realm\sound\InsRealSlow.mp3]])
+LSM:Register("sound", "|cff00ffffRealm - Torch:Moan|r", [[Interface\Addons\SharedMedia_Realm\sound\TorchMoan.mp3]])
+LSM:Register("sound", "|cff00ffffRealm - Torch:Spread|r", [[Interface\Addons\SharedMedia_Realm\sound\TorchSpread.mp3]])
+LSM:Register("sound", "|cff00ffffRealm - Humble:Hero|r", [[Interface\Addons\SharedMedia_Realm\sound\HumbleHero.mp3]])
+LSM:Register("sound", "|cff00ffffRealm - VictoryScream|r", [[Interface\Addons\SharedMedia_Realm\sound\HippoScream.mp3]])
+
+-- -----
+--   STATUSBAR
+-- -----
